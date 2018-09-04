6fc35f8e74976da9b5e7092f88a12beabf3e9fab
ZnHoCn <me@hakr.xyz>
2018-09-04 16:14:39 +0800

    Modify stc-header

---
+++ examples/stc-header/src/main.c
@@ -0,0 +1,10 @@
+//#include <reg51.h>		// old header from SDCC
+//#include "STC15Fxx.h"     // Official header from STC-ISP for STC15F204EA / STC15F104E
+//#include "STC15F104E.h"   // Official header from STC-ISP for STC15Wxx / STC15Fxx
+//#include "STC89xx.h"      // Official header from STC-ISP for STC89xx / STC90xx
+
+void main()
+{
+    /* code */
+    return 0;
+}
