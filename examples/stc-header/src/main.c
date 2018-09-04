ea446c9078d41b3d93deca057012a3defc93573a
ZnHoCn <me@hakr.xyz>
2018-09-04 16:20:11 +0800

    Modify stc-header

--- examples/stc-header/src/main.c
+++ examples/stc-header/src/main.c
@@ -1,4 +1,4 @@
-//#include <reg51.h>		// old header from SDCC
+#include <reg51.h>		// old header from SDCC
 //#include "STC15Fxx.h"     // Official header from STC-ISP for STC15F204EA / STC15F104E
 //#include "STC15F104E.h"   // Official header from STC-ISP for STC15Wxx / STC15Fxx
 //#include "STC89xx.h"      // Official header from STC-ISP for STC89xx / STC90xx
